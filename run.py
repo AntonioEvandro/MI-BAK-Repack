40ce219d74c9cb5c2d703f65747cd80502503cf3
Evandro <evandroalves123321@gmail.com>
2026-09-02 10:00:16 -0300

    Update run.py

--- run.py
+++ run.py
@@ -3,6 +3,5 @@ from functions.CreateBak import BakMi
 if __name__ == "__main__":
     inputFiles = r"inputs"
     outputs = r"outputs"
-    fileName = input("Nome do arquivo final: ") + r".bak"
+    fileName = input("Nome do arquivo final: ")
     BakMi(inputFiles, outputs, fileName)
-    print("\nArquivos em: " + inputFiles + ", usados para criar: " + fileName + ".\nPasta do .bak resultante: " + outputs + ".")
