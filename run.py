ac0a86063687f0eb394d2408155924a182b38d1f
Evandro <evandroalves123321@gmail.com>
2026-09-02 09:43:28 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
 if __name__ == "__main__":
     inputFiles = r"inputs"
     outputs = r"outputs"
+    fileName = input("Nome do arquivo final: ") + r".bak"
+    print("\nArquivos em: " + inputFiles + ", usados para criar: " + fileName + ".\nPasta do .bak resultante: " + outputs + ".")
