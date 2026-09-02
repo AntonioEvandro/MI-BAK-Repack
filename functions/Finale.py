a4b56374520e11ff572b0f14e1488543ec4ca341
Evandro <evandroalves123321@gmail.com>
2026-09-02 12:11:18 -0300

    Melhorias visuais

--- functions/Finale.py
+++ functions/Finale.py
@@ -1,6 +1,14 @@
 from utils.Utils import sucess
 
 def finale(inputs, name, outputs, size):
-    print(sucess())
-    print(f"\nArquivos em: " + inputs + ", usados para criar: " + name + r".bak" + ".\nVeja o resultado em: " + outputs + ".")
-    print(f"\nTamanho total: {size}.\nNão esqueça de usar o tamanho total em bytes nos campos do descript.xml antes de zipar.")
+
+    finale =  f"""
+                ════════════════════════════════
+                    Arquivos em: \033[33m{inputs}\033[0m      
+                    usados para criar:
+                        \033[32m{name + r".bak"}\033[0m.
+                    Tamanho total: \033[32m{size}\033[0m.
+                    Veja o resultado em: \033[33m{outputs}\033[0m.
+
+                ════════════════════════════════"""
+    print(sucess(), finale + "\n\033[31mNão esqueça\033[0m de usar o\033[32m tamanho total em bytes\033[0m nos campos do\033[34m descript.xml\033[0m antes de zipar.")
