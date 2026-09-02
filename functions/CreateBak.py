4824a1bab34abb264232a9fb79a237e504e3566b
Evandro <evandroalves123321@gmail.com>
2026-09-02 10:09:04 -0300

    Update CreateBak.py
    Aplicação de compressão GZIP

--- functions/CreateBak.py
+++ functions/CreateBak.py
@@ -1,5 +1,6 @@
 import os
 import tarfile
+import gzip
 
 def BakMi(inputs, outputs, name):
     """
@@ -7,10 +8,14 @@ def BakMi(inputs, outputs, name):
     """
     tarTemp = outputs + "/" + name + ".tar"
 
-    print("1. Criando a estrutura de arquivos TAR...")
+    print("1. Criando a estrutura de arquivos...")
     with tarfile.open(tarTemp, "W") as tar:
         tar.add(inputs, os.path.basename(inputs))
 
-    
+    print("2. Aplicando compressão...")
+    with open(tarTemp, "rb") as fIn:
+        tarData = fIn.read()
+
+    gzipData = gzip.compress(tarData)
 
     print("\nArquivos em: " + inputs + ", usados para criar: " + name + r".bak" + ".\nPasta do .bak resultante: " + outputs + ".")
