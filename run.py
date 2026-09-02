43fda0bae0286df3cc9b2169ddd8f652aebc9565
Evandro <evandroalves123321@gmail.com>
2026-09-02 10:59:43 -0300

    Update run.py

--- run.py
+++ run.py
@@ -3,5 +3,3 @@ from functions.CreateBak import BakMi
 if __name__ == "__main__":
     inputFiles = r"inputs"
     outputs = r"outputs"
-    fileName = input("Nome do arquivo final: ")
-    BakMi(inputFiles, outputs, fileName)
