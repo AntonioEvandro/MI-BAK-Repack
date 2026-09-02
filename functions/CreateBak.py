a59330e68571385c2237498eb3940670571c2ed9
Evandro <evandroalves123321@gmail.com>
2026-09-02 10:02:11 -0300

    Update CreateBak.py

--- functions/CreateBak.py
+++ functions/CreateBak.py
@@ -1,7 +1,13 @@
+import tarfile
+
 def BakMi(inputs, outputs, name):
     """
         Empacota uma pasta de dados no formato de backup compátivel com o MIUI.
     """
     tarTemp = outputs + "/" + name + ".tar"
+
+    print("1. Criando a estrutura de arquivos TAR...")
+    with tarfile.open(tarTemp, "W") as tar:
+        tar.add(inputs)
 
     print("\nArquivos em: " + inputs + ", usados para criar: " + name + r".bak" + ".\nPasta do .bak resultante: " + outputs + ".")
